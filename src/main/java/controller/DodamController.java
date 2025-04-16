b4aac545beba45a4cbff34e4e6b225d7b6d810df
hyeon0114 <kshyeon114@gmail.com>
2025-04-16 11:38:26 +0900

    [SN-1908] test3 메서드 추가
    - 매서드 추가

--- src/main/java/controller/DodamController.java
+++ src/main/java/controller/DodamController.java
@@ -29,5 +29,14 @@ public class DodamController {
         String testef32 = "하늘보다 넘어짐 왜 수정 안됨";
     }
 
+    public void test3(){
+        String test = "kek";
+        String teste = "kek";
+        String testf = "kek";
+        String testef = "kek";
+
+        String testef32 = "하늘보다 넘어짐 왜 수정 안됨";
+    }
+
 
 }
