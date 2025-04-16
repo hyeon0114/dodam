96d723c22efd914fa3f929849ca2a22b6a78fb99
hyeon0114 <kshyeon114@gmail.com>
2025-04-16 11:37:06 +0900

    [SN-1900] test2 메서드 추가
    - 매서드 추가

--- src/main/java/controller/DodamController.java
+++ src/main/java/controller/DodamController.java
@@ -20,5 +20,14 @@ public class DodamController {
         String testef32 = "하늘보다 넘어짐 왜 수정 안됨";
     }
 
+    public void test2(){
+        String test = "kek";
+        String teste = "kek";
+        String testf = "kek";
+        String testef = "kek";
+
+        String testef32 = "하늘보다 넘어짐 왜 수정 안됨";
+    }
+
 
 }
