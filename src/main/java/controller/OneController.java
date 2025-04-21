c62c88f99655e7c1e62a3d0c6e9a558b356a3fd4
hyeon0114 <kshyeon114@gmail.com>
2025-04-21 17:11:55 +0900

    [SN-1000] 브랜치 분기 테스트
    - 브랜치 분기 테스트, 1000

---
+++ src/main/java/controller/OneController.java
@@ -0,0 +1,19 @@
+package controller;
+
+public class OneController {
+    public void teset () {
+        String a = "test";
+    }
+
+    public void teset1 () {
+        String a = "test";
+    }
+
+    public void teset2 () {
+        String a = "test";
+    }
+
+    public void teset4 () {
+        String a = "test";
+    }
+}
