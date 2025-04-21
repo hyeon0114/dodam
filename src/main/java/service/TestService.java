18f2947026bb46ca33ad1826253f05ddd2363b17
hyeon0114 <kshyeon114@gmail.com>
2025-04-21 14:11:26 +0900

    [SN-1887] test3 메서드 추가
    - 수정

---
+++ src/main/java/service/TestService.java
@@ -0,0 +1,11 @@
+package service;
+
+import repository.DodamRepository;
+
+public class TestService {
+    private final DodamRepository dodamRepository;
+
+    public TestService(DodamRepository dodamRepository) {
+        this.dodamRepository = dodamRepository;
+    }
+}
