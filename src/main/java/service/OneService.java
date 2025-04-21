cf2c9102cfe0d997ad500327ce9ac5a5abfff468
hyeon0114 <kshyeon114@gmail.com>
2025-04-21 17:13:11 +0900

    [SN-1001] 브랜치 분기 테스트
    - 브랜치 분기 테스트, 1001

---
+++ src/main/java/service/OneService.java
@@ -0,0 +1,9 @@
+package service;
+
+public class OneService {
+    private final DodamService dodamService;
+
+    public OneService(DodamService dodamService) {
+        this.dodamService = dodamService;
+    }
+}
